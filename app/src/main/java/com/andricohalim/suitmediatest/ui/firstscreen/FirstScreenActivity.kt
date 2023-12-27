f8dc30940daf0f0a9176e799b1e165902c5c9100
Andrico <103520236+AndricoHalim@users.noreply.github.com>
2023-12-27 13:52:47 +0700

    Add ispalindrome function

--- app/src/main/java/com/andricohalim/suitmediatest/ui/firstscreen/FirstScreenActivity.kt
+++ app/src/main/java/com/andricohalim/suitmediatest/ui/firstscreen/FirstScreenActivity.kt
@@ -3,6 +3,7 @@ package com.andricohalim.suitmediatest.ui.firstscreen
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.andricohalim.suitmediatest.R
 import com.andricohalim.suitmediatest.databinding.ActivityFirstScreenBinding
 import com.andricohalim.suitmediatest.ui.secondscreen.SecondScreenActivity
@@ -18,10 +19,39 @@ class FirstScreenActivity : AppCompatActivity() {
 
         supportActionBar?.hide()
 
-        binding.btnNext.setOnClickListener {
-            val intent = Intent(this, SecondScreenActivity::class.java)
-            startActivity(intent)
+        binding.apply {
+            btnNext.setOnClickListener {
+                val intent = Intent(this@FirstScreenActivity, SecondScreenActivity::class.java)
+                startActivity(intent)
+            }
+            btnCheck.setOnClickListener {
+                val inputText = edPalindrome.text.toString()
+                val isPalindrome = isPalindrome(inputText)
+                if (isPalindrome) {
+                    Toast.makeText(applicationContext, "isPalindrome", Toast.LENGTH_SHORT).show()
+                } else{
+                    Toast.makeText(applicationContext, "not Palindrome", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
+    }
 
+    private fun isPalindrome(str: String): Boolean {
+        var start = 0
+        var end = str.length - 1
+        while (start < end) {
+            while (start < end && !str[start].isLetterOrDigit()) {
+                start++
+            }
+            while (start < end && !str[end].isLetterOrDigit()) {
+                end--
+            }
+            if (str[start] != str[end]) {
+                return false
+            }
+            start++
+            end--
+        }
+        return true
     }
 }
